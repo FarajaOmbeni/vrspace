28c3a6a42239876f68931b451799cf0d03cc24dc
myles4321 <mylesadebayo@gmail.com>
2024-06-07 23:34:21 +0300

    resources page

--- resources/js/Pages/Experiences.vue
+++ resources/js/Pages/Experiences.vue
@@ -1,13 +1,31 @@
 <template>
 <Layout>
     <Head title="Experiences"/>
-    <div>
-        <h1>Experiences</h1>
-        <p>Here are some of my experiences</p>
-    </div>
+        <div class="bg-cover bg-center h-screen mb-16" style="background-image: url('/images/experiences/experiences.png');">
+            <div class="h-screen flex flex-col items-center justify-center text-6xl font-extrabold text-blue">
+                <p class="text-pink">VR SPACE</p>
+                <p class="text-purple">-</p>
+                <p class="text-purple">EXPERIENCE</p>
+            </div>
+        </div>
+
+        <ExperienceOdd image_link="/images/experiences/htc.png"
+            title="HTC VIVE VR"
+            image="/images/experiences/user.png"
+            player="1 Player/Time"
+            time="10 Minutes/Round"
+            price="KES 400"
+            description="Rich and diversified games, suitable for people of all ages."
+            d1="Boxing games"
+            d2="Shooting games"
+            d3="Horror games"
+            d4="Children games"
+             />
 </Layout>
 </template>
  
 <script setup>
 import Layout from '@/Layouts/Layout.vue';
+import ExperienceOdd from '@/Shared/ExperienceOdd.vue';
+import ExperienceEven from '@/Shared/ExperienceEven.vue';
 </script>
---
+++ resources/js/Shared/ExperienceOdd.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex justify-center xl:gap-32 mb-32 md:gap-14">
+      <div class="hidden w-[25rem] xl:w-[30rem] ml-12 lg:block flex justify-center lg:none"><img :src="image_link" alt="">
+      </div>
+      <div class="lg:w-1/2 mx-8 text-justify lg:text-start flex flex-col items-center lg:w-[20rem] lg:justify-center lg:items-start bg-purple rounded-2xl">
+        <p class="mb-6 lg:mb-3 text-2xl p-2">{{ title }}</p>
+        <div class="flex p-3">
+          <img class="w-[2rem] md:w-[5rem]" :src="image" alt="">
+          <div>
+            <p class=" text-lg">{{ player }}</p>
+            <p class=" text-lg">{{ time }}</p>
+            <p class=" text-lg">{{ price }}</p>
+          </div>
+        </div>
+        <p class="lg:p-3 text-lg">{{ description }}</p>
+        <ul class="p-3">
+          <li class="text-lg">{{ d1 }}</li>
+          <li class="text-lg">{{ d2 }}</li>
+          <li class="text-lg">{{ d3 }}</li>
+          <li class="text-lg">{{ d4 }}</li>
+        </ul>
+
+
+      </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  image_link: String,
+  title: String,
+  image: String,
+  player: String,
+  time: String,
+  price: String,
+  description: String,
+  d1: String,
+  d2: String,
+  d3: String,
+  d4: String,
+});
+</script>
